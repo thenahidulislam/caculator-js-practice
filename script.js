72cfa5bf2c17c339f63477952a9b3c18c7a3bfc2
Nahidul Islam <nahidulislam1417@gmail.com>
2024-07-27 18:34:22 +0600

    Some comments added.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
+    //e means event
     button.addEventListener('click', (e) =>{
         if(e.target.innerHTML == '='){
             string = eval(string);
